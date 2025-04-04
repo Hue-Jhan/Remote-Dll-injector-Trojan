acc683005b6aa26ff1b03c8c7e13be8e8ed1052c
Hue <75088696+Hue-Jhan@users.noreply.github.com>
2025-04-04 16:43:46 +0200

    Update injectorPack2.cpp

--- code/injector/remote/injectorPack2.cpp
+++ code/injector/remote/injectorPack2.cpp
@@ -8,38 +8,28 @@ void ExtractEmbeddedDLL() {
     HRSRC hRes = FindResource(NULL, MAKEINTRESOURCE(IDR_DLL2), RT_RCDATA);
     if (hRes == NULL) {
         printf("Failed to find DLL resource.\n");
-        return;
-    }
-    else { printf("a"); }
+        return;    }
 
     DWORD dwSize = SizeofResource(NULL, hRes);
     if (dwSize == 0) {
         printf("Failed to get size of DLL resource.\n");
-        return;
-    }
-    else { printf("b"); }
+        return;    }
 
     HGLOBAL hGlobal = LoadResource(NULL, hRes);
     if (hGlobal == NULL) {
         printf("Failed to load DLL resource.\n");
-        return;
-    }
-    else { printf("c"); }
+        return;    }
 
     void* pData = LockResource(hGlobal);
     if (pData == NULL) {
         printf("Failed to lock resource.\n");
-        return;
-    }
-    else { printf("d"); }
-
+        return;    }
   
     FILE* file = NULL;
     errno_t err = fopen_s(&file, "extracted.dll", "wb");
     if (err != 0) {
         printf("Failed to create output file.\n");
-        return;
-    }
+        return;    }
 
     fwrite(pData, 1, dwSize, file);
     fclose(file);
@@ -77,53 +67,46 @@ void InjectDLL(const char* targetProcessName) {
     DWORD pid = GetProcessID(targetProcessName);
     if (pid == 0) {
         printf("Failed to get PID of target process.\n");
-        return;
-    }
+        return;    }
 
-
-
-    HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
+    HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);   // may have to replace with (PROCESS_VM_OPERATION | PROCESS_VM_WRITE | PROCESS_VM_READ, FALSE, pid) to avoid permission errors
     if (hProcess == NULL) {
         printf("Failed to open target process.\n");
-        return;
-    }
-
+        return;    }
 
     LPVOID pRemoteMem = VirtualAllocEx(hProcess, NULL, strlen("extracted.dll") + 1, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
     if (pRemoteMem == NULL) {
         printf("Failed to allocate memory in target process.\n");
-        return;
-    }
-    else { printf("a"); }
-
+        return;    }
 
     char fullDllPath[MAX_PATH];
     if (GetFullPathNameA("extracted.dll", MAX_PATH, fullDllPath, NULL) == 0) {
         printf("Failed to retrieve full DLL path. Error code: %lu\n", GetLastError());
-        return;
-    }
-    else { printf("b"); }
+        return;    }
+    
     GetFullPathNameA("extracted.dll", MAX_PATH, fullDllPath, NULL);
     WriteProcessMemory(hProcess, pRemoteMem, fullDllPath, strlen(fullDllPath) + 1, NULL);
-
 
     HMODULE kernel32Base = GetModuleHandleW(L"kernel32.dll");
     if (kernel32Base == NULL) {
         printf("Failed to retrieve handle to kernel32.dll: %d\n", GetLastError());
-        return;
-    }
-    else { printf("c"); }
+        return;    }
+    
     FARPROC load_library_address = GetProcAddress(kernel32Base, "LoadLibraryA");
-
+    if (load_library_address == NULL) {
+        printf("Failed to get address of LoadLibraryA. Error code: %lu\n", GetLastError());
+        return;    }
 
     HANDLE hThread = CreateRemoteThread(hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)load_library_address, pRemoteMem, 0, NULL);
     if (hThread == NULL) {
         printf("Failed to create remote thread.\n");
-        return;
-    }
-    else { printf("d"); }
+        return;    }
 
-    WaitForSingleObject(hThread, INFINITE);
+    if (WaitForSingleObject(hThread, INFINITE) == WAIT_FAILED) {
+        printf("Failed to wait for remote thread. Error code: %lu\n", GetLastError());
+    return;    
+    }
+
     CloseHandle(hThread);
     CloseHandle(hProcess);
 }
